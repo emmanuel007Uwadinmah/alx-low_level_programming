caa183d494ca3abef30f7be71d44dbb490a5c89f
emmanuel007uwadinmah <emmanueluwadinmah@gmail.com>
2023-02-21 04:48:06 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/holberton.h
@@ -0,0 +1,28 @@
+# 1 "0-holberton.c"
+# 1 "<built-in>"
+# 1 "<command-line>"
+# 31 "<command-line>"
+# 1 "/usr/include/stdc-predef.h" 1 3 4
+# 32 "<command-line>" 2
+# 1 "0-holberton.c"
+# 1 "holberton.h" 1
+# 2 "0-holberton.c" 2
+
+
+
+
+
+int main(void)
+{
+ char str[] = "Holberton\n";
+ int i = 0;
+
+ while (str[i] != '\0')
+   {
+     char c = str[i];
+
+      _putchar(c);
+       i++;
+        }
+ return (0);
+}
