5b101e06e9fde9fff93ee70393945b51d78dbebe
emmanuel007uwadinmah <emmanueluwadinmah@gmail.com>
2023-04-09 12:17:03 -0700

    1-create_file.c task completed

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * Prototype: int create_file(const char *filename, char *text_content);
+ * where filename is the name of the file to create and text_content is a NULL terminated string to write to the file
+ * Returns: 1 on success, -1 on failure (file can not be created, file can not be written, write “fails”, etc…)
+ * The created file must have those permissions: rw-------. If the file already exists, do not change the permissions.
+ * if the file already exists, truncate it.
+ * if filename is NULL return -1.
+ * if text_content is NULL create an empty file.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int o, w, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(o, text_content, len);
+
+	if (o == -1 || w == -1)
+		return (-1);
+
+	close(o);
+
+	return (1);
+}
