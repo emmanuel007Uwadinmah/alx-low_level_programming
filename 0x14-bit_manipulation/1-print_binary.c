3305a7e8ae8f421a0151529cfa43a557b53a2cf0
emmanuel007uwadinmah <emmanueluwadinmah@gmail.com>
2023-04-09 13:09:30 -0700

    1-print_binary.c task completed

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Prototype: void print_binary(unsigned long int n);
+ * Format: see example
+ * You are not allowed to use arrays
+ * You are not allowed to use malloc
+ * You are not allowed to use the % or / operators
+ */
+void print_binary(unsigned long int n)
+{
+	if (n > 1)
+		print_binary(n >> 1);
+
+	_putchar((n & 1) + '0');
+}
