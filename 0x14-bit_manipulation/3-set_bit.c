b2ec73212aee4b17c4a96fc38b3c8a7c2da95daa
emmanuel007uwadinmah <emmanueluwadinmah@gmail.com>
2023-04-10 00:23:44 -0700

    3-set_bit.c task completed

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * Prototype: int set_bit(unsigned long int *n, unsigned int index);
+ * where index is the index, starting from 0 of the bit you want to set
+ * Returns: 1 if it worked, or -1 if an error occurred.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= (sizeof(unsigned long int) * 8))
+		return (-1);
+
+	*n ^= (1 << index);
+
+	return (1);
+}
